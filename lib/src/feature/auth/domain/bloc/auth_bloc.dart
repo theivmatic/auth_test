bcf1c0bed318d76f5947f3a15eacd68382329b5f
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-01-31 00:21:28 +0500

    added states to auth bloc

--- lib/src/feature/auth/domain/bloc/auth_bloc.dart
+++ lib/src/feature/auth/domain/bloc/auth_bloc.dart
@@ -1,5 +1,5 @@
-import 'package:bloc/bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'auth_event.dart';
--- lib/src/feature/auth/domain/bloc/auth_state.dart
+++ lib/src/feature/auth/domain/bloc/auth_state.dart
@@ -4,3 +4,21 @@ part of 'auth_bloc.dart';
 sealed class AuthBlocState {}
 
 final class AuthBlocInitialState extends AuthBlocState {}
+
+final class AuthBlocLoadingState extends AuthBlocState {}
+
+final class AuthBlocLoadedState extends AuthBlocState {}
+
+final class AuthBlocErrorState extends AuthBlocState {
+  final String error;
+
+  AuthBlocErrorState({required this.error});
+}
+
+final class PhoneAuthCodeSentSuccessState extends AuthBlocState {
+  final String verificationId;
+
+  PhoneAuthCodeSentSuccessState({required this.verificationId});
+}
+
+final class OtpSuccessState extends AuthBlocState {}
