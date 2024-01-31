222344b5c77480977a7b2b0f4a11127617e7e012
Александр <alexandrfuss@MacBook-Air-Aleksandr.local>
2024-01-31 15:01:40 +0500

    registred auth bloc in main file and added bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:auth_test/observer.dart';
 import 'package:auth_test/src/core/routes/app_router.dart';
+import 'package:auth_test/src/feature/auth/domain/bloc/auth_bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  Bloc.observer = AppObserver();
   runApp(const MainApp());
 }
 
@@ -13,8 +17,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter().config(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AuthBloc(),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter().config(),
+      ),
     );
   }
 }
